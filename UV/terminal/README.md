c67101097df6840973df6aed5b0a45df75f8c00b
beyg1 <abdulrubbaig1@gmail.com>
2025-04-29 15:30:46 +0500

    Simple terminal based openai sdk agent made with uv

---
+++ UV/terminal/README.md
@@ -0,0 +1,9 @@
+simple uv proj setup to work with openai sdk
+1. uv init terminal
+2. cd terminal
+3. uv venv => activate v.env
+4. uv add openai-agents 
+5. uv add python-dotenv
+
+To run main.py from .toml script you should run uv run main.py
+its a simple ai app to run a response by an Ai agent on  console/terminal.
--- UV/terminal/main.py
+++ UV/terminal/main.py
@@ -4,34 +4,33 @@ from agents import set_default_openai_client
 import os
 from dotenv import load_dotenv
 
-def main():
- set_tracing_disabled(True)   # Disable tracing for the agent chat completion as for llms other than openai this gives an error
- load_dotenv()
+set_tracing_disabled(True)   # Disable tracing for the agent chat completion as for llms other than openai this gives an error
+load_dotenv()
 
- gemini_api_key = os.getenv("GOOGLE_API_KEY")    # dotenv getting api key from .env file
+gemini_api_key = os.getenv("GOOGLE_API_KEY")    # dotenv getting api key from .env file
 
- external_client = AsyncOpenAI( # diff llm models then gpt for openai sdk
+external_client = AsyncOpenAI( # diff llm models then gpt for openai sdk
     api_key=gemini_api_key,    
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
  )
- set_default_openai_client(external_client) 
+set_default_openai_client(external_client) 
 
- model = OpenAIChatCompletionsModel(
+model = OpenAIChatCompletionsModel(
     model="gemini-2.0-flash", # selecting the model to use for the llm call
     openai_client=external_client
  )
 
- agent: Agent = Agent(name="Assistant", instructions="You are a helpful assistant for an ai application", model=model) #agent with instructions and model
+agent: Agent = Agent(name="Assistant", instructions="You are a helpful assistant for an ai application", model=model) #agent with instructions and model
 
- result = Runner.run_sync(agent, "say Hello world ") # running the agent with the input prompt
+result = Runner.run_sync(agent, "say Hello world ") # running the agent to do hello world, as a test case
  
- print(result.final_output) # outputting the final result of the agent's response
+print(result.final_output) # outputting the final result of the agent's response
 
- #interactive chat loop
+#interactive chat loop
  
- history = []
- # [{"role": "user", "content": "?"}, {"role": "assistant", "content": ""}]
- while True:
+history = []
+# [{"role": "user", "content": "?"}, {"role": "assistant", "content": ""}]
+while True:
         # Input
         user_input = input("Enter your message or simply Type 'exit' to quit: ")
         
@@ -51,4 +50,3 @@ def main():
 
         print("assistant: ", response.final_output)
 
-main()
