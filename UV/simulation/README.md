0c11643850f37b3a87ec88b73311de828e5a0001
beyg1 <abdulrubbaig1@gmail.com>
2025-06-15 10:59:18 +0500

    class code project

---
+++ UV/simulation/README.md
@@ -0,0 +1,5 @@
+uv init  --to initialize uv project
+uv add openai-agents
+copy agent level code from 04_hello_agent
+copy path of .venv and press ctr + shift + p and at add path, paste it to remove errors of modules.
+uv run main.py
---
+++ UV/simulation/main.py
@@ -0,0 +1,34 @@
+import asyncio
+import os
+from dotenv import load_dotenv
+from openai import AsyncOpenAI
+from agents import Agent, OpenAIChatCompletionsModel, Runner, set_tracing_disabled
+
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+#Reference: https://ai.google.dev/gemini-api/docs/openai
+client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+set_tracing_disabled(disabled=True)
+
+async def main():
+    # This agent will use the custom LLM provider
+    agent = Agent(
+        name="Assistant",
+        instructions="You only respond in haikus.",
+        model=OpenAIChatCompletionsModel(model="gemini-2.5-flash-preview-05-20", openai_client=client),
+    )
+
+    result = await Runner.run(
+        agent,
+        "Tell me about recursion in programming.",
+    )
+    print(result.final_output)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
